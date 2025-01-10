695ae6c4b280804746b6d993efd9246b7814eb4f
adam <asadowsk@uoguelph.ca>
2025-01-09 21:31:03 -0500

    Rewrite preface

--- render.R
+++ render.R
@@ -2,18 +2,11 @@ library(bookdown)
 
 serve_book(dir = ".", output_dir = "_book", preview = TRUE, quiet = FALSE)
 
-unlink("_main.Rmd")
-
-bookdown::publish_book()
+bookdown::publish_book(name = "rtist")
 
 Y
+
 preview_chapter("03-opening-data.rmd")
-
-
-
-
-
-
 
 render_book(input = ".", output_format = NULL, clean = TRUE,
             envir = parent.frame(),
